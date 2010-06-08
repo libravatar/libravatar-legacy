81853125feecd9409f083e98e45b3069aa94af35
Francois Marier <fmarier@gmail.com>
2010-06-08 22:04:34 +1200

    Create a very simple homepage

--- libravatar/urls.py
+++ libravatar/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^account/', include('libravatar.account.urls')),
     (r'^tools/', include('libravatar.tools.urls')),
+    (r'^$', 'libravatar.public.views.home'),
 
     (r'^admin/', include(admin.site.urls)),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
