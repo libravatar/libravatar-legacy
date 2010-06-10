669463da7fed1b2141f3c90afc211833c9d09c9a
Francois Marier <fmarier@gmail.com>
2010-06-10 15:37:38 +1200

    Redirect to homepage after logout and fix signup link on homepage

--- libravatar/account/urls.py
+++ libravatar/account/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     ('login/$', 'django.contrib.auth.views.login', {'template_name': 'account/login.html'}),
-    ('logout/$', 'django.contrib.auth.views.logout_then_login'),
+    ('logout/$', 'django.contrib.auth.views.logout', {'next_page' : '/'}),
     ('password_change/$', 'django.contrib.auth.views.password_change', {'template_name': 'account/password_change.html'}),
     ('password_change_done/$', 'django.contrib.auth.views.password_change_done', {'template_name': 'account/password_change_done.html'}),
 
