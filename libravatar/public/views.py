30fd41800a18dfe45cc0d5645f83473d0aab239a
Francois Marier <fmarier@gmail.com>
2010-06-27 19:37:32 +1200

    Extend the resolver to maintain the "d" parameter and fix escaping problems in the mod_rewrite file

--- libravatar/public/views.py
+++ libravatar/public/views.py
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Libravatar.  If not, see <http://www.gnu.org/licenses/>.
 
+import urllib
+
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
@@ -65,6 +67,13 @@ def resolve(request):
         return render_to_response('public/resolve_nohash.html',
                                   context_instance=RequestContext(request))
 
+    # Maintain the default redirection that was specified
+    not_found = ''
+    if 'd' in request.GET:
+        not_found = '?d=%s' % urllib.quote(request.GET['d'])
+    elif 'default' in request.GET:
+        not_found = '?d=%s' % urllib.quote(request.GET['default'])
+
     email_hash = request.GET['email_hash']
     avatar_server = settings.SITE_URL
 
@@ -73,5 +82,8 @@ def resolve(request):
         delegation_server = lookup_avatar_server(request.GET['domain'])
         if delegation_server:
             avatar_server = delegation_server
+            # redirect first to 
+            not_found = '?d=' + urllib.quote(settings.AVATAR_URL + email_hash + not_found)
 
-    return HttpResponseRedirect(avatar_server + '/avatar/' + email_hash)
+    final_url = avatar_server + '/avatar/' + email_hash + not_found
+    return HttpResponseRedirect(final_url)
