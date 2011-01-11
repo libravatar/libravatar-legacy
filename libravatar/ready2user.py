14cbc7653d91e0b5ab1c0585b16d916437a5b82c
Francois Marier <fmarier@gmail.com>
2011-01-11 17:16:29 +1300

    Remove unnecessary import (copied from cropresize)

--- libravatar/ready2user.py
+++ libravatar/ready2user.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Libravatar.  If not, see <http://www.gnu.org/licenses/>.
 
-import Image
 import json
 import os
 import shutil
