2c3f6d98169ee1ba30e5e0453e7a2aab22c554af
Francois Marier <fmarier@gmail.com>
2011-03-31 23:18:12 +1300

    Disable photo import when confirming email but not logged in
    Before this, email confirmation as a logged out user was working but it
    resulted in a nasty 500 error in libravatar/account/views.py, line 99:
    
      NoReverseMatch: Reverse for 'libravatar.account.views.import_photo'
      with arguments '(None,)' and keyword arguments '{}' not found.

--- libravatar/account/views.py
+++ libravatar/account/views.py
@@ -87,12 +87,13 @@ def confirm_email(request):
 
     external_photos = []
 
-    identica = identica_photo(confirmed.email)
-    if identica:
-        external_photos.append(identica)
-    gravatar = gravatar_photo(confirmed.email)
-    if gravatar:
-        external_photos.append(gravatar)
+    if not request.user.is_anonymous():
+        identica = identica_photo(confirmed.email)
+        if identica:
+            external_photos.append(identica)
+            gravatar = gravatar_photo(confirmed.email)
+            if gravatar:
+                external_photos.append(gravatar)
 
     return render_to_response('account/email_confirmed.html',
                               {'email_id' : confirmed.id, 'photos' : external_photos},
