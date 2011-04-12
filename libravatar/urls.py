7b4d6345ca6b16fd3a08a3d36e3c5955a139a188
Francois Marier <fmarier@gmail.com>
2011-04-12 20:23:30 +1200

    Disable all admin-related things

--- libravatar/urls.py
+++ libravatar/urls.py
@@ -18,9 +18,6 @@
 # pylint: disable=W0401,W0614
 from django.conf.urls.defaults import *
 
-from django.contrib import admin
-admin.autodiscover()
-
 urlpatterns = patterns('',
     (r'^account/', include('libravatar.account.urls')),
     (r'^tools/', include('libravatar.tools.urls')),
