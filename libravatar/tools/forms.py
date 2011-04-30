a4c61ef24e4e59c7dad5c357e7911521b2321dd2
Francois Marier <fmarier@gmail.com>
2011-04-30 20:49:33 +1200

    Translate 'tools' application

--- libravatar/tools/forms.py
+++ libravatar/tools/forms.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2010  Francois Marier <francois@libravatar.org>
+# Copyright (C) 2010, 2011  Francois Marier <francois@libravatar.org>
 #
 # This file is part of Libravatar
 # 
@@ -16,22 +16,23 @@
 # along with Libravatar.  If not, see <http://www.gnu.org/licenses/>.
 
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 from libravatar.account.forms import MIN_LENGTH_URL, MAX_LENGTH_URL
 
 class CheckForm(forms.Form):
-    email = forms.EmailField(required=False)
-    openid = forms.URLField(required=False, verify_exists=False, min_length=MIN_LENGTH_URL, max_length=MAX_LENGTH_URL, label='OpenID')
-    size = forms.DecimalField(required=False, min_value=1, max_value=512, decimal_places=0, initial=80)
-    not_found = forms.CharField(required=False, label='Default URL')
+    email = forms.EmailField(label=_('Email'), required=False)
+    openid = forms.URLField(label=_('OpenID'), required=False, verify_exists=False, min_length=MIN_LENGTH_URL, max_length=MAX_LENGTH_URL)
+    size = forms.DecimalField(label=_('Size'), required=False, min_value=1, max_value=512, decimal_places=0, initial=80)
+    not_found = forms.CharField(label=_('Default URL'), required=False)
 
     def clean(self):
         cleaned_data = self.cleaned_data
         if not cleaned_data.get('email') and not cleaned_data.get('openid'):
-            raise forms.ValidationError('You must provide a valid email or OpenID')
+            raise forms.ValidationError(_('You must provide a valid email or OpenID'))
         if cleaned_data.get('email') and cleaned_data.get('openid'):
-            raise forms.ValidationError('You cannot provide both an email and an OpenID. Choose one!')
+            raise forms.ValidationError(_('You cannot provide both an email and an OpenID. Choose one!'))
         return cleaned_data
 
 class CheckDomainForm(forms.Form):
-    domain = forms.CharField()
+    domain = forms.CharField(label=_('Domain'))
