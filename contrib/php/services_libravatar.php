9a03ca7643cec908da4e146da1989985267d2227
Francois Marier <fmarier@gmail.com>
2011-05-22 22:27:49 +1200

    Add a test page for Melissa's PHP class

---
+++ contrib/php/services_libravatar.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ * Test for the Services_Libravatar PEAR package
+ *
+ * You must have symlinks to the library in the same directory as this file.
+ *
+ * e.g.
+ *  ln -s ~/devel/remote/services_libravatar/Services /var/www/
+  */
+
+ini_set('display_errors', true);
+ini_set('display_startup_errors', true);
+ini_set('error_reporting', E_ALL);
+ini_set('html_errors', true);
+
+require_once 'Services/Libravatar.php';
+
+$libravatar = new Services_Libravatar();
+
+$avatar_url = $libravatar->url('fmarier@gmail.com');
+$missing_avatar = $libravatar->url('fmarier+1@gmail.com');
+
+print 'Regular HTTP images:<br>';
+print '<img src="' . $avatar_url . '">';
+print '<img src="' . $missing_avatar . '">';
+print "<br><br>\n";
+
+$avatar_url = $libravatar->url('fmarier@gmail.com', array('https' => 1));
+$missing_avatar = $libravatar->url('fmarier+1@gmail.com', array('https' => 1));
+
+print 'Regular HTTPS images:<br>';
+print '<img src="' . $avatar_url . '">';
+print '<img src="' . $missing_avatar . '">';
+print "<br><br>\n";
+
+$avatar_url = $libravatar->url('francois@catalyst.net.nz');
+$missing_avatar = $libravatar->url('francois+1@catalyst.net.nz');
+
+print 'Federated HTTP images:<br>';
+print '<img src="' . $avatar_url . '">';
+print '<img src="' . $missing_avatar . '">';
+print "<br><br>\n";
+
+$avatar_url = $libravatar->url('francois@catalyst.net.nz', array('https' => 1));
+$missing_avatar = $libravatar->url('francois+1@catalyst.net.nz', array('https' => 1));
+
+print 'Federated HTTPS images:<br>';
+print '<img src="' . $avatar_url . '">';
+print '<img src="' . $missing_avatar . '">';
+print "<br><br>\n";
+
+$avatar_url = $libravatar->url('https://launchpad.net/~fmarier');
+$missing_avatar = $libravatar->url('http://launchpad.net/~notfmarier');
+
+print 'Regular HTTP images (OpenID):<br>';
+print '<img src="' . $avatar_url . '">';
+print '<img src="' . $missing_avatar . '">';
+print "<br><br>\n";
