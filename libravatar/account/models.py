2163517de3c2a80ecb34c42638295b2a041f91ee
Francois Marier <fmarier@gmail.com>
2011-09-25 16:52:19 +1300

    Update copyright headers and remove unnecessary whitespace in them

--- libravatar/account/models.py
+++ libravatar/account/models.py
@@ -4,21 +4,21 @@
 #                     Brett Wilkins <bushido.katana@gmail.com>
 #
 # This file is part of Libravatar
-# 
+#
 # Libravatar is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
 # by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # Libravatar is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with Libravatar.  If not, see <http://www.gnu.org/licenses/>.
 #
-# This file incorporates work covered by the following copyright and  
+# This file incorporates work covered by the following copyright and
 # permission notice:
 #
 #     Copyright (c) 2009, Simon Willison
