f143d5ce5acaef7cd7c2946b5c3bc914eda27078
Francois Marier <fmarier@gmail.com>
2011-12-12 22:29:51 +1300

    Catch another type of error during OpenID discovery (bug #902896)
    Also adds some debugging output to the error log to help reproduce the
    error if necessary.

--- libravatar/account/views.py
+++ libravatar/account/views.py
@@ -274,7 +274,12 @@ def redirect_openid(request, openid_id):
 
     try:
         auth_request = openid_consumer.begin(user_url)
-    except consumer.DiscoveryFailure, exception:
+    except consumer.DiscoveryFailure as exception:
+        print "OpenID discovery failed (userid=%s) for %s" % (request.user.id, user_url)
+        return render_to_response('account/openid_discoveryfailure.html', {'message': exception},
+                                  context_instance=RequestContext(request))
+    except UnicodeDecodeError as exception:
+        print "OpenID discovery failed (userid=%s) for %s" % (request.user.id, user_url)
         return render_to_response('account/openid_discoveryfailure.html', {'message': exception},
                                   context_instance=RequestContext(request))
 
