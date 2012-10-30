fce7767ba5f8ada2591a88f9034dba438c70535b
Francois Marier <francois@debian.org>
2012-10-30 20:25:41 +1300

    Convert the slave hourly cronjob to run every 30 min
    The move to a crontab will allow us to redirect the output to a
    different email address as per bug #1072680.

--- debian/libravatar-slave.cron.d
+++ debian/libravatar-slave.cron.d
@@ -1,1 +1,2 @@
 */15 * * * *     libravatar-slave       test -e /usr/bin/libravatar-update-mirror && /usr/bin/libravatar-update-mirror
+*/30 * * * *     root                   test -e /usr/sbin/libravatar-update-certs && /usr/sbin/libravatar-update-certs
